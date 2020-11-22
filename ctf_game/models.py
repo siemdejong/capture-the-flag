6a2b51f239d2e3be790873169a1af373a5efe7fa
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2020-11-22 16:58:01 +0100

    add add_points() to the Team model

--- ctf_game/models.py
+++ ctf_game/models.py
@@ -1,17 +1,37 @@
 from django.db import models
+from django.db.models import Sum
 
 # Create your models here.
 class Team(models.Model):
     name = models.CharField(max_length=50)
-    score = models.FloatField()
+    score = models.FloatField(default=0)
+
+    def add_points(self):
+        captured_flags = Flag.objects.filter(owner_id=self.id)
+        earned_points = captured_flags.aggregate(Sum('worth'))['worth__sum']
+        self.score += earned_points
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    def __repr__(self):
+        return f"{self.name} ({self.score})"
 
 class Flag(models.Model):
     name = models.CharField(max_length=50)
     worth = models.FloatField()
-    bonus_on_capture = models.FloatField()
     owner = models.ForeignKey(
         Team,
         on_delete=models.SET_NULL,
         blank=True,
         null=True
     )
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    def __repr__(self):
+        if self.owner:
+            return f"{self.name} ({self.worth}) | {self.owner}"
+        else:
+            return f"{self.name} ({self.worth})"
--- ctf_game/tests.py
+++ ctf_game/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Team, Flag
 
 # Create your tests here.
+class TeamModelTest(TestCase):
+    def test_add_points(self):
+        # Setup the database.
+        team_one = Team(name='team_one')
+        team_one.save()
+        Flag.objects.create(name='flag_one', worth=1, owner=team_one)
+
+        # Add up all the points associated with the captured flags.
+        team_one.add_points()
+        self.assertEqual(team_one.score, 1)
